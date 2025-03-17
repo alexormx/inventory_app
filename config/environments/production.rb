a3d3eb2fb4913e4600d5c5628979412e7e57b196
Alex Ortiz <alexormx@gmail.com>
2025-03-17 01:15:53 -0600

    Change the port to fix connection with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '465',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => 'apikey',
     :password       => ENV['SENDGRID_API_KEY'],
