875aebd5348f7194058e3f308883e75f3e7fa70a
Alex Ortiz <alexormx@gmail.com>
2025-03-19 00:12:37 -0600

    fix precomple error2

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,9 +3,6 @@
 //= link_tree ../stylesheets .css
 //= link application.js
 //= link @hotwired/turbo-rails
-//= link_tree ../../javascript/controllers .js
+//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
-//= link_tree ../../javascript/custom .js
-//= link_tree ../../javascript/components .js
-//= link_tree ../../javascript/modules .js
-//= link_tree ../../javascript/utilities .js
+
