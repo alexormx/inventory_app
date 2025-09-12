30d47f7835513eb81271a441bd55e08d91875a75
Alex Ortiz <alexormx@gmail.com>
2025-09-12 01:30:15 -0500

    modify the migrations to assure appropiate usage

--- db/migrate/20250906103000_add_sale_order_item_id_to_inventories.rb
+++ db/migrate/20250906103000_add_sale_order_item_id_to_inventories.rb
@@ -1,6 +1,11 @@
 class AddSaleOrderItemIdToInventories < ActiveRecord::Migration[8.0]
   def change
-    add_column :inventories, :sale_order_item_id, :integer
-    add_index  :inventories, :sale_order_item_id
+    unless column_exists?(:inventories, :sale_order_item_id)
+      add_column :inventories, :sale_order_item_id, :integer
+    end
+
+    unless index_exists?(:inventories, :sale_order_item_id)
+      add_index  :inventories, :sale_order_item_id
+    end
   end
 end
