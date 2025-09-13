7c6f545ba603ea0e66afb23df27ac121f681f8c9
Alex Ortiz <alexormx@gmail.com>
2025-09-13 11:47:22 -0500

    test: sign in confirmed user and init cart session for CartItemsController spec

--- spec/controllers/cart_items_controller_spec.rb
+++ spec/controllers/cart_items_controller_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe CartItemsController, type: :controller do
   let!(:product) { create(:product, status: :active) }
+  let(:user) { create(:user, confirmed_at: Time.current) }
+
+  before do
+    sign_in user
+    session[:cart] = {}
+  end
 
   describe "POST #create" do
     it "adds a product to the cart" do
-  post :create, params: { product_id: product.id }
-  expect(session[:cart]).to be_present
+      post :create, params: { product_id: product.id }
+      expect(session[:cart]).to be_present
       expect(session[:cart][product.id.to_s]).to eq(1)
     end
   end
