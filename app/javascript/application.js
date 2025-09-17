cb9346f9c4dd34bea3310dce9675bb0496676bd9
Alex Ortiz <alexormx@gmail.com>
2025-09-16 23:19:37 -0500

    fix(js): importar scripts de órdenes y navbar_shrink en entrypoint; reactivar cálculos de totales y shrink del navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,6 +4,13 @@ import "./controllers"   // index.js arranca Stimulus y registra todo
 // Legacy helpers still in vanilla
 import "./custom/cart_preview"
 
+// Órdenes: cálculo de costos y totales (SO/PO)
+import "./components/order_items"
+import "./components/purchase_order_items"
+
+// Navbar: comportamiento de reducción de altura al hacer scroll
+import "./custom/navbar_shrink"
+
 // Production: disable verbose debug
 window.APP_DEBUG = false
 
