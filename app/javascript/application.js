060940415b9a175c8b2b260388bfb82c67cdb445
Alex Ortiz <alexormx@gmail.com>
2025-09-16 23:28:06 -0500

    chore(js): importar módulos faltantes (navbar_toggle, theme_toggle, tooltip_init, cart_preview_dynamic, inventory_adjustment_lines, password validation)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "./controllers"   // index.js arranca Stimulus y registra todo
 
 // Legacy helpers still in vanilla
 import "./custom/cart_preview"
+import "./custom/cart_preview_dynamic"
 
 // Órdenes: cálculo de costos y totales (SO/PO)
 import "./components/order_items"
@@ -10,6 +11,19 @@ import "./components/purchase_order_items"
 
 // Navbar: comportamiento de reducción de altura al hacer scroll
 import "./custom/navbar_shrink"
+// Navbar: hamburguesa/collapse sin Bootstrap JS
+import "./custom/navbar_toggle"
+// Tema claro/oscuro (persistencia en localStorage)
+import "./custom/theme_toggle"
+// Tooltips (si hay data-bs-toggle="tooltip")
+import "./custom/tooltip_init"
+
+// Admin: ajustes de inventario (form dinámico)
+import "./components/inventory_adjustment_lines"
+
+// Devise: validación y requisitos de contraseña
+import "./components/password_validation"
+import "./components/show_password_requirements"
 
 // Production: disable verbose debug
 window.APP_DEBUG = false
